826a2a88112fd4afb59d18b6434f4d0bc011b849
Torido Hikki <nhu.truong213@gmail.com>
2019-09-20 18:01:05 +0700

    sticky-div(not) and  slide images(not)

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function btnOnClick(event) {
         errorEmail.innerHTML = "Please enter your Email!";
         event.preventDefault();
     }
-    if (messElement.value ==="" || messElement.value.length < 128) {
+    if (messElement.value === "") {
         var errorMess = document.getElementById("error-mess");
         errorMess.innerHTML = "Please enter your messages!";
         event.preventDefault();
@@ -24,5 +24,38 @@ function inputEmail(){
 }
 function inputMessage(){
     var errorMess = document.getElementById("error-mess");
-    errorMess.innerHTML = "";
+    errorMess.innerHTML ="";
+}
+/*function nullValue() {
+    document.getElementById("mess").focus();
+    document.getElementById("mess").value=null();
+}
+*/
+
+//Slide Images
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
